7065a4af5484a9e465fd5bccc17e236e57257814
kunyu <1960049949@qq.com>
2025-09-28 15:31:45 +0800

    初始化

--- __init__.py
+++ __init__.py
@@ -1,9 +1,9 @@
 # __init__.py
 from .OpenAIApi import OpenAIApi, NODE_CLASS_MAPPINGS as AI_NodeMap, NODE_DISPLAY_NAME_MAPPINGS as AI_NameMap
-from .OpenAIApiEdit import OpenAIApiEdit, NODE_CLASS_MAPPINGS as AI_NodeMap, NODE_DISPLAY_NAME_MAPPINGS as AI_NameMap
+from .OpenAIApiEdit import OpenAIApiEdit, NODE_CLASS_MAPPINGS as AI_EDIT_NodeMap, NODE_DISPLAY_NAME_MAPPINGS as AI_EDIT_NameMap
 from .CustomText import CustomText, NODE_CLASS_MAPPINGS as Text_NodeMap, NODE_DISPLAY_NAME_MAPPINGS as Text_NameMap
 from .MultiImageURLNode import MultiImageURLNode, NODE_CLASS_MAPPINGS as Multi_NodeMap, NODE_DISPLAY_NAME_MAPPINGS as Multi_NameMap
 
 # 合并所有节点映射
-NODE_CLASS_MAPPINGS = {**AI_NodeMap, **Text_NodeMap, **Multi_NodeMap}
-NODE_DISPLAY_NAME_MAPPINGS = {**AI_NameMap, **Text_NameMap, **Multi_NameMap}
+NODE_CLASS_MAPPINGS = {**AI_NodeMap,**AI_EDIT_NodeMap, **Text_NodeMap, **Multi_NodeMap}
+NODE_DISPLAY_NAME_MAPPINGS = {**AI_NameMap,**AI_EDIT_NameMap, **Text_NameMap, **Multi_NameMap}
