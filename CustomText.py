2b8a27b3f47fe376c1bd9011d7a1d91b27c3ddb7
kunyu <1960049949@qq.com>
2025-10-15 17:56:21 +0800

    初始化

--- CustomText.py
+++ CustomText.py
@@ -6,18 +6,20 @@ class CustomText:
             "required": {
                 # "text": ("STRING", {"multiline": True}),
                 "text": ("STRING", {"default": True}),
+                "text2": ("STRING", {"multiline": True}),
             }
         }
 
-    RETURN_TYPES = ("STRING",)
+    RETURN_TYPES = ("STRING","STRING")
     FUNCTION = "generate"
     CATEGORY = "Semir Hoo ai"
 
     def generate(self, **kwargs):
         # 获取输入文本
         text = kwargs.get("text")
+        text2 = kwargs.get("text2")
         # 这里可以做文本处理、模板替换等
-        return (text,)
+        return (text,text2)
 
 # 注册节点
 NODE_CLASS_MAPPINGS = {"CustomText": CustomText}
